2fc2160f7caeb1a6160f9f669dd4a9d37bb6e926
lhb <hbb0b0@163.com>
2018-01-15 16:51:03 +0800

    apring mvc support return jason data

--- maven01/src/main/java/com/maven01/controller/DemoController.java
+++ maven01/src/main/java/com/maven01/controller/DemoController.java
@@ -1,8 +1,14 @@
 package com.maven01.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.maven01.dto.*;
 @Controller
 @RequestMapping("/mvc")
 public class DemoController {
@@ -11,4 +17,18 @@ public class DemoController {
     public String hello(){        
         return "index";
     }
+    
+   
+    @RequestMapping(method=RequestMethod.GET, value="/getUser",produces="application/json")
+    public @ResponseBody List<UserDto> getUser(){        
+    	
+    	ArrayList<UserDto> resultList=new ArrayList<UserDto>();
+    	for(int i=0;i<100;i++)
+    	{
+            UserDto user=new UserDto(i,String.format("hbb0b0-%s", i+""));
+            resultList.add(user);
+    	}
+        return resultList;
+    }
+    
 }
